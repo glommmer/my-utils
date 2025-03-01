ea0a7d9f90ac870fe8e7e0bf210d2fcb1621ecb3
glommmer <glommer@kakao.com>
2025-03-02 08:21:57 +0900

    Add to_single_line()

--- my_utils/__init__.py
+++ my_utils/__init__.py
@@ -1,4 +1,5 @@
 from .remove_sql_comments import remove_sql_comments
 from .get_outer_brackets import get_outer_brackets
+from .to_single_line import to_single_line
 
-__all__ = ["remove_sql_comments", "get_outer_brackets"]
+__all__ = ["remove_sql_comments", "get_outer_brackets", "to_single_line"]
---
+++ my_utils/to_single_line.py
@@ -0,0 +1,47 @@
+import re
+from my_utils import get_outer_brackets
+
+
+def to_single_line(text: str) -> str:
+    text = re.sub(r"\\[\n\r]+", " ", text)
+    text = re.sub(r"\s+\.", ".", text)
+
+    multiline_pattern = re.compile(r'""".*?"""', flags=re.DOTALL)
+    multiline_matches = multiline_pattern.findall(text)
+    for match in multiline_matches:
+        singleline = re.sub(r"\n\s*", " ", match)
+        tmp_match_asis = re.search(r'"""(.+)"""', singleline).group(1)
+        if '"' in tmp_match_asis:
+            tmp_match_tobe = tmp_match_asis.replace('"', "'")
+            singleline = singleline.replace(tmp_match_asis, tmp_match_tobe, 1)
+        text = text.replace(match, singleline)
+    text = text.replace('"""', '"')
+    text = re.sub(r" +", " ", text)
+
+    bracket_objects = get_outer_brackets(text)
+    if bracket_objects:
+        for obj in bracket_objects:
+            obj = re.escape(obj)
+            match = re.search(obj, text, flags=re.DOTALL).group()
+            if "\n" in match:
+                singleline = match.replace("\n", " ")
+                text = text.replace(match, singleline, 1)
+    return text
+
+
+if __name__ == "__main__":
+    foo = """
+        \"\"\"
+        SELECT *
+          FROM TABLE
+        \"\"\"
+        spark = SparkSession\
+                .builder\
+                .getOrCreate()
+        s3.head_object(
+            Bucket="My-Bucket",
+            Key="path/to/key",
+        )
+    """
+    var = to_single_line(foo)
+    print(var)
